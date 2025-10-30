720d7566ef321715e387742026f43328ccbd1112
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-10-30 10:36:32 -0500

    Delete ProjectGenesis directory

--- ProjectGenesis/includes/sections/main/404.php
+++
@@ -1,9 +0,0 @@
-<div class="section-content <?php echo ($CURRENT_SECTION === '404') ? 'active' : 'disabled'; ?>" data-section="404">
-    <div class="not-found-container">
-        <div class="not-found-badge">404</div>
-        <h1 class="not-found-title"><?php echo __('404.title'); ?></h1>
-        <p class="not-found-text">
-            <?php echo __('404.message'); ?>
-        </p>
-    </div>
-</div>
